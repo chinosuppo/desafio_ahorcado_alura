bd74a80c4d65a11dee3977d86743bb8beecf251e
Carlos <carlitosakd@gmail.com>
2022-02-19 18:06:25 -0300

    Cambios realizados

--- ahorcado.js
+++ ahorcado.js
@@ -1,127 +1,178 @@
-var listaDeSelecciones = ["URUGUAY", "ITALIA", "BRASIL", "ALEMANIA","INGLATERRA", "ARGENTINA", "FRANCIA", "ESPAÑA", "SUECIA", "CHILE", "MEXICO", "SUIZA", "ESTADOS UNIDOS", "COREA DEL SUR", "JAPON", "SUDAFRICA", "RUSIA", "QATAR"];
+var listaDeSelecciones = ["URUGUAY", "ITALIA", "BRASIL", "ALEMANIA","INGLATERRA", "ARGENTINA", "FRANCIA", "ESPAÑA", "SUECIA", "CHILE", "MEXICO", "SUIZA", "EEUU", "COREA", "JAPON", "SUDAFRICA", "RUSIA", "QATAR"];
 var botonInicio = document.querySelector("#iniciar-juego");
+var inputSeleccion = document.querySelector("#input-nueva-seleccion");
 var botonAgregarSeleccion = document.querySelector("#nueva-seleccion");
-var botonIngresarLetra = document.querySelector("#ingresar-letra");
+//var botonIngresarLetra = document.querySelector("#ingresar-letra");
+var botonReinicio = document.querySelector("#reiniciar-juego");
 
-var letrasCorrectas = "";
-var letrasIncorrectas = "";
-var seleccionElegida = "";
-var totalDeLetrasElegidas = "";
-var sigueElJuego = true;
-var inicio = false;
+var inicioDelJuego = false;
+var seleccionAleatoria;
+var indices = [];
+var arraySeleccion;
+var arrayLetraIngresante = [];
+var arrayLetrasCorrectas = [];
+var arrayLetrasIncorrectas = [];
+let letrasUnicas = [];
 
-function elegirSeleccionAleatoria(lista) {
-    seleccionAleatoria = Math.random()*lista.length;
-    return lista[Math.floor(seleccionAleatoria)];
-}
-
-function guionesPorSeleccion(seleccion) {
-    var x = 50;
-    var y = 50;
-
-    for(var i = 0; i < seleccion.length; i++){
-        dibujarGuion(x,y);
-        x = x + 50;
+function filtrarLetrasRepetidas(){
+    for(var i = 0; i < seleccionAleatoria.length; i++){
+        if(!letrasUnicas.includes(seleccionAleatoria[i])){
+            letrasUnicas.push(seleccionAleatoria[i]);
+        }
     }
 }
 
-function iniciar() {
-    limpiarTablero();
-    seleccionElegida = elegirSeleccionAleatoria(listaDeSelecciones);
-    guionesPorSeleccion(seleccionElegida);
-    letrasIncorrectas="";
-    totalDeLetrasElegidas = "";
-    muerto = 0;
-    inicio = false;
-    
+function sortearSeleccion(){
+    var numeroAleatorio = Math.floor(Math.random()*listaDeSelecciones.length);
+    seleccionAleatoria = listaDeSelecciones[numeroAleatorio];
+    listaDeSelecciones.splice(numeroAleatorio,1);
+    return seleccionAleatoria;
 }
 
-botonInicio.addEventListener("click",function(event){
-  event.preventDefault();
-  iniciar();
-  inicio = true;
+function crearArraySeleccion(seleccion){
+    separada = seleccion.split("");
+    arraySeleccion = separada;
+}
 
+botonInicio.addEventListener("click", function(event){
+    event.preventDefault();
+    pincel.clearRect(0,0,pantalla.width,pantalla.height);
+    iniciarJuego();
 });
 
-botonAgregarSeleccion.addEventListener("click",function(event){
+botonReinicio.addEventListener("click", function(event){
     event.preventDefault();
-    var seleccionIngresante = document.querySelector("#input-nueva-seleccion");
-    seleccionIngresante = seleccionIngresante.toUpperCase();
+    pincel.clearRect(0,0,pantalla.width,pantalla.height);
+    iniciarJuego();
+});
 
-    if(listaDeSelecciones.includes(seleccionIngresante)) {
-        alert("La seleccion ingresada por usted ya existe en el juego");
-    } else {
-        listaDeSelecciones.push(seleccionIngresante);
-        alert("Se agrego la seleccion al juego");
+botonAgregarSeleccion.addEventListener("click", function(event){
+    event.preventDefault();
+    listaDeSelecciones.push(inputSeleccion.value.toUpperCase());
+    inputSeleccion.value = "";
+    inputSeleccion.focus();
+});
+
+function dibujarGuion(){
+    var inicioX = 350;
+    var inicioY = 610;
+    var contador = 0;
+    var nLetras = seleccionAleatoria.length;
+    while(contador < nLetras){
+        pincel.fillStyle = "black";
+        pincel.fillRect(inicioX + (40*contador), inicioY,30,4);
+        contador++;
+    }   
+}
+
+function iniciarJuego(){
+    pincel.clearRect(0,0,pantalla.width,pantalla.height);
+    pizarron();
+    sortearSeleccion();
+    crearArraySeleccion(seleccionAleatoria);
+    dibujarGuion();
+    filtrarLetrasRepetidas();
+    inicioDelJuego = true;
+    arrayLetraIngresante = [];
+    arrayLetrasCorrectas = [];
+    arrayLetrasIncorrectas = [];
+}
+
+function buscarIndices(){
+    if(inicioDelJuego){
+        var indiceBuscado = arraySeleccion.indexOf(arrayLetraIngresante[0]);
+        while(indiceBuscado != -1){
+            indices.push(indiceBuscado);
+            indiceBuscado = arraySeleccion.indexOf(arrayLetraIngresante[0], indiceBuscado + 1);
+        }
     }
-    document.querySelector("#input-nueva-seleccion").value = "";
+}
 
+function dibujarLetras(arrOrden){
+    var inicioX = 358;
+    var inicioY = 600;
+    for(var i = 0; i < arrOrden.length; i++){
+        pincel.fillStyle = "#265813";
+        pincel.font = "25px Arial";
+        pincel.fillText(arrayLetraIngresante[0], inicioX+(40*arrOrden[i]),inicioY);
+    }
+    indices = [];
+}
+
+document.addEventListener("keyup", function(event){
+    arrayLetraIngresante = [];
+    var letra = event.key.toUpperCase();
+    var codigo = letra.charCodeAt();
+    if(inicioDelJuego){
+        if(codigo>64 && codigo<91){
+            arrayLetraIngresante.push(letra);
+            buscarIndices();
+            dibujarLetras(indices);
+            var comparar = arrayLetrasIncorrectas.length;
+            if(arraySeleccion.includes(letra)){
+                if(!arrayLetrasCorrectas.includes(letra)){
+                    arrayLetrasCorrectas.push(letra);
+                    dibujarLetras(letra);
+                }
+            }else if(!arrayLetrasIncorrectas.includes(letra)){
+                arrayLetrasIncorrectas.push(letra);
+            }
+            if(comparar < arrayLetrasIncorrectas.length){
+                dibujarLetrasIncorrectas(arrayLetrasIncorrectas);
+            }
+            dibujarAhorcado();
+        }
+        verificarGanador();
+        verificarPerdedor();
+    }
 });
 
-botonIngresarLetra.addEventListener("click",function(event){
-    event.preventDefault();
+function dibujarLetrasIncorrectas(letrasIncorrectas){
+    var inicioX = 400;
+    var inicioY = 200;
+    pincel.fillStyle = "red";
+    pincel.font = "30px Verdana";
+    pincel.fillText("Letras incorrectas " + letrasIncorrectas.toString(), inicioX, inicioY);
+}
 
-   if(sigueElJuego && inicio) {
-       var letraIngresante = document.querySelector("#input-letra").value;
-       var repeat = false;
+function verificarGanador(){
+    let seleccionSinRepetirLetras = letrasUnicas.sort().toString();
+    let letrasIncorrectasIngresadas = arrayLetrasCorrectas.sort().toString();
+    if(seleccionSinRepetirLetras === letrasIncorrectasIngresadas){
+        pincel.fillStyle = "green";
+        pincel.font = "50px Arial";
+        pincel.fillText("HAS GANADO!!!", 600,400);
+        inicioDelJuego = false;
+        botonReinicio.focus();
+        letrasUnicas = [];
+    }
+}
 
-       if(letraIngresante.length == 1) {
-           letraIngresante = letraIngresante.toUpperCase();
+function verificarPerdedor(){
+    if(arrayLetrasIncorrectas.length>5){
+        pincel.fillStyle = "red";
+        pincel.font = "50px Helvetica";
+        pincel.fillText("HAS PERDIDO!", 600,400);
+        inicioDelJuego = false;
+        alert("La seleccion era: " + seleccionAleatoria);
+        botonReinicio.focus();
+        letrasUnicas = [];
+    }
+}
 
-            if(totalDeLetrasElegidas = "") {
-                totalDeLetrasElegidas = totalDeLetrasElegidas + letraIngresante;
-            }else{
-
-                for(var i = 0; i < totalDeLetrasElegidas.length; i++) {
-                    if(totalDeLetrasElegidas[i] == letraIngresante){
-                        repeat = true;
-                        alert("La letra ya esta elegida, elige otra");
-                        break;
-                    }
-                }
-
-                if(repeat == false) {
-
-                    totalDeLetrasElegidas = totalDeLetrasElegidas + letraIngresante;
-
-                }
-            }
-           
-            if((dibujarLetraCorrecta(seleccionElegida, letraIngresante) == 1) && (repeat = false)) {
-                letrasIncorrectas = letrasIncorrectas + letraIngresante;
-                dibujarLetrasIncorrectas(letrasIncorrectas);
-            }else {
-                letrasCorrectas = letrasCorrectas + letraIngresante;
-            }
-            
-            if(letrasCorrectas.length == seleccionElegida.length) {
-                muerto = 2;
-                sigueElJuego = false;
-                inicio = false;
-            }
-
-            if (letrasIncorrectas.length == 8) {
-                muerto = 1;
-                sigueElJuego = false;
-                inicio = false;
-            }else {
-                //alert("Solo ingresar una letra");
-            }
-
-            document.querySelector("#input-letra").value = "";
-       }else {
-           alert("Iniciar nuevo juego!!!");
-           location.reload();
-       }
-
-       if(muerto == 1) {
-           pincel.fillStyle = "red";
-           pincel.fillText("USTED ES UN PERDEDOR", 450, 100);
-       }else if(muerto == 2) {
-           pincel.fillStyle = "green";
-           pincel.fillText("USTED ES UN GANADOR", 450, 100);
-       }
-
-   }
-
-});
+function dibujarAhorcado(){
+    let contador = arrayLetrasIncorrectas.length;
+    if(contador===1){
+        cabeza();
+    }else if(contador===2){
+        cuerpo();
+    }else if(contador===3){
+        brazoIzquierdo();
+    }else if(contador===4){
+        brazoDerecho();
+    }else if(contador===5){
+        piernaIzquierda();
+    }else if(contador===6){
+        piernaDerecha();
+    }
+}
--- canvas.js
+++ canvas.js
@@ -1,151 +1,68 @@
-var pantalla = document.getElementById("ahorcado");
+var pantalla = document.querySelector("#ahorcado");
 var pincel = pantalla.getContext("2d");
-var muerto = 0;
 
-function dibujarGuion(x,y) {
-    pincel.fillStyle = "blue";
-    pincel.fillRect(x,y,20,1);
+
+function pizarron(){
+    pincel.fillStyle = "black"
+    pincel.beginPath();
+    pincel.moveTo(100,600);
+    pincel.lineTo(50,620);
+    pincel.lineTo(150,620);
+    pincel.fill();
+    pincel.fillRect(95,205,10,400);
+    pincel.fillRect(95,205,200,10);
+    pincel.fillRect(295,205,10,50);
+}
+function cabeza(){
+    pincel.fillStyle = "black"
+    pincel.beginPath();
+    pincel.arc(300,295,40,0,2*3.14);
     pincel.fill();
 }
 
-function dibujarLetraCorrecta(seleccion,letra) {
-    var distancia = 50;
-    var devolver = 1;
-
-    pincel.font = "25px Arial";
-    pincel.fillStyle = "black";
-
-    for(var i = 0; i < seleccion.length; i++){
-        if(seleccion[i] == letra) {
-            pincel.fillText(seleccion[i],distancia,40);
-            devolver = 0;
-        }
-    
-    distancia = distancia + 50;
-    pincel.moveTo(distancia,400);
-    }
-
-    pincel.stroke();
-    return devolver;
+function cuerpo(){
+    pincel.fillStyle = "black"
+    pincel.fillRect(295,335,10,130);
 }
 
-function dibujarColumna() {
-    pincel.moveTo(150,500);
-    pincel.lineWidth = 4;
-    pincel.lineTo(100,550);
-    pincel.lineTo(200,550);
-    pincel.closePath();
-    pincel.stroke();
-
-    pincel.moveTo(150,500);
-    pincel.moveTo(150,200);
-    pincel.stroke();
-}
-
-function dibujarSoga() {
-    pincel.moveTo(150,200);
-    pincel.lineTo(300,200);
-    pincel.lineTo(300,250);
-    pincel.stroke();
-}
-
-function dibujarCabeza() {
+function brazoIzquierdo(){
+    pincel.fillStyle = "black"
     pincel.beginPath();
-    pincel.arc(300,270,0,Math.PI*2,true);
-    pincel.stroke();
+    pincel.moveTo(295,355);
+    pincel.lineTo(220,325);
+    pincel.lineTo(220,335);
+    pincel.lineTo(295,365);
+    pincel.fill();
 }
 
-function dibujarCuerpo() {
-    pincel.moveTo(300,420);
-    pincel.lineTo(250,500);
-    pincel.stroke();
+function brazoDerecho(){
+    pincel.fillStyle = "black"
+    pincel.beginPath();
+    pincel.moveTo(305,355);
+    pincel.lineTo(380,325);
+    pincel.lineTo(380,335);
+    pincel.lineTo(305,365);
+    pincel.fill();
 }
 
-function dibujarPiernaIzq() {
-    pincel.moveTo(300,420);
-    pincel.lineTo(250,500);
-    pincel.stroke();
+function piernaIzquierda(){
+    pincel.fillStyle = "black"
+    pincel.beginPath();
+    pincel.moveTo(295,455);
+    pincel.lineTo(210,505);
+    pincel.lineTo(210,515);
+    pincel.lineTo(295,465);
+    pincel.fill();
 }
 
-function dibujarPiernaDer() {
-    pincel.moveTo(300,420);
-    pincel.lineTo(350,500);
-    pincel.stroke();
-}
-
-function dibujarBrazoIzq() {
-    pincel.moveTo(300,360);
-    pincel.lineTo(230,290);
-    pincel.stroke();
-}
-
-function dibujarBrazoDer() {
-    pincel.moveTo(300,360);
-    pincel.lineTo(370,290);
-    pincel.stroke();
-}
-
-function dibujarFinal(texto) {
-    pincel.font = "50px Helvetica";
-        if(texto == "Perdiste!! Fin del juego") {
-            pincel.fillStyle = "red";
-        }else{
-            pincel.fillStyle = "blue";
-        }
-        pincel.fillText(texto,520,350);
-        pincel.stroke();
-}
-
-function dibujarLetrasIncorrectas(letrasIncorrectas) {
-    var distancia = 50;
-    pincel.font = "25px Arial";
-    pincel.fillStyle = "#b33417";
-
-    for(var y = 0; x < letrasIncorrectas.length; y++){
-        pincel.fillText(letrasIncorrectas[y],distancia,80);
-        distancia = distancia + 50;
-        pincel.moveTo(distancia,400);
-    }
-    pincel.stroke();
-
-    switch(letrasIncorrectas.length) {
-        case 1: 
-                dibujarColumna();
-                break; 
-    
-        case 1: 
-                dibujarColumna();
-                break;
-        
-        case 2: 
-                dibujarSoga();
-                break;
-                
-        case 3: 
-                dibujarCabeza();
-                break;
-    
-        case 4: 
-                dibujarCuerpo();
-                break;
-    
-        case 5: 
-                dibujarBrazoIzq();
-                break;
-    
-        case 6: 
-                dibujarBrazoDer();
-                break;
-    
-        case 7: 
-                dibujarPiernaIzq();
-                break;
-    
-        case 8: 
-                dibujarPiernaDer();
-                break;
-    }
-    
+function piernaDerecha(){
+    pincel.fillStyle = "black"
+    pincel.beginPath();
+    pincel.moveTo(305,455);
+    pincel.lineTo(390,505);
+    pincel.lineTo(390,515);
+    pincel.lineTo(305,465);
+    pincel.fill();
 }
 
 function limpiarTablero() {
